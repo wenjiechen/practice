5361f4e4b10f2e12d820485dfc74aafa566f8409
wenjie chen <wenjie.chen@nyu.edu>
2014-01-05 10:35:02 -0500

    add profile

---
+++ src/chapter4Tree/Questions.java
@@ -0,0 +1,11 @@
+package chapter4Tree;
+
+import chapter4Tree.BinarySearchTree.Node;
+
+public class Questions {
+
+  // 4.1 check a tree is blanced
+//  public static boolean isBlanced(Node root) {
+//    
+//  }
+}
