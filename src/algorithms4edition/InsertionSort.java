5f9a21d3c1ae6cfe5851c316e574e3257e0659e8
wenjie chen <wenjie.chen@nyu.edu>
2014-01-09 01:39:03 -0500

    organize

--- src/algorithms4edition/InsertionSort.java
+++ src/algorithms4edition/InsertionSort.java
@@ -15,7 +15,6 @@ public class InsertionSort {
   }
 
   private static void show(Comparable[] a) {
-
     for (Comparable i : a) {
       System.out.print(i + " ");
     }
@@ -31,12 +30,41 @@ public class InsertionSort {
     return true;
   }
 
-  public static void sort(Comparable[] a) {
-    for (int i = 0; i < a.length; i++) {
+  public static void insertionSort(Comparable[] a) {
+    for (int i = 1; i < a.length; i++) {
+      // insert a[i] into a[i-1],a[i-2]...
       for (int j = i; j > 0; j--) {
         if (less(a[j], a[j - 1]))
           exch(a, j, j - 1);
+        else
+          // because the left elements of i are in order
+          break;
       }
+    }
+  }
+
+  public static void selectionSort(Comparable[] a) {
+    int N = a.length;
+    for (int i = 0; i < N; i++) {
+      int min = i;
+      for (int j = i; j < N; j++) {
+        if (less(a[j], a[min]))
+          min = j;
+      }
+      exch(a, i, min);
+    }
+  }
+
+  public static void sortMethod(String sort, Comparable[] a) {
+    switch (sort) {
+    case "insertion":
+      System.out.println("using insertion sort");
+      insertionSort(a);
+      break;
+    case "selection":
+      System.out.println("using selection sort");
+      selectionSort(a);
+      break;
     }
   }
 
@@ -47,7 +75,7 @@ public class InsertionSort {
       test[i] = rand.nextInt(50);
     }
     show(test);
-    sort(test);
+    sortMethod("insertion",test);
     show(test);
     System.out.println(isSorted(test));
 
