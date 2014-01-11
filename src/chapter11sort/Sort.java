b84c68186570fdde33561006faf9a1abddb8087a
wenjie chen <wenjie.chen@nyu.edu>
2014-01-11 14:42:54 -0500

    merge reverse

--- src/chapter11sort/Sort.java
+++ src/chapter11sort/Sort.java
@@ -2,4 +2,33 @@ package chapter11sort;
 
 public class Sort {
 
+  /**
+   * question 11.1 merge to sorted array a and b into a.
+   * 
+   * @param a
+   * @param b
+   * @param lengthA
+   * @param lengthB
+   */
+  public static void mergeAB(int[] a, int[] b, int lengthA, int lengthB) {
+    int lastA = lengthA - 1;
+    int lastB = lengthB - 1;
+    int lastIndex = lengthA + lengthB - 1;
+    int i = lastA;
+    int j = lastB;
+    int k = lastIndex;
+    while (i >= 0 && j >= 0) {
+      if (a[i] > b[j])
+        a[k--] = a[i--];
+      else
+        a[k--] = b[j--];
+    }
+    while (i >= 0) {
+      a[k--] = a[i--];
+    }
+    while (j >= 0) {
+      a[k--] = b[j--];
+    }
+  }
+
 }
--- src/chapter11sort/SortTest.java
+++ src/chapter11sort/SortTest.java
@@ -1,5 +1,51 @@
 package chapter11sort;
 
-public class SortTest {
+import java.util.Arrays;
+import java.util.Random;
 
+public class SortTest {
+  private static void println(Object x) {
+    System.out.println(x);
+  }
+
+  private static void print(Object x) {
+    System.out.print(x);
+  }
+
+  private static boolean isSorted(int[] a) {
+    for (int i = 1; i < a.length; i++) {
+      if (a[i] < a[i-1])
+        return false;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Random rand = new Random(47);
+    int lengthB = rand.nextInt(20);
+    int lengthA = rand.nextInt(15);
+    int[] a = new int[lengthA];
+    int[] b = new int[lengthB];
+    for (int i = 0; i < lengthA; i++) {
+      a[i] = rand.nextInt(50);
+    }
+    Arrays.sort(a);
+    println("array a");
+    println(Arrays.toString(a));
+    for (int i = 0; i < lengthB; i++) {
+      b[i] = rand.nextInt(100);
+    }
+    Arrays.sort(b);
+    println("array b");
+    println(Arrays.toString(b));
+    int[] c = new int[lengthA + lengthB];
+    for (int i = 0; i < lengthA; i++) {
+      c[i] = a[i];
+    }
+    a = c;
+    Sort.mergeAB(a, b, lengthA, lengthB);
+    println("merge ab");
+    println(Arrays.toString(a));
+    println(isSorted(a));
+  }
 }
