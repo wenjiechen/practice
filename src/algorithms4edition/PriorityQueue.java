cc5939a98f76d0d9a2923fc95dcb2260dbad390e
wenjie chen <wenjie.chen@nyu.edu>
2014-01-11 01:02:37 -0500

    heap sort

--- src/algorithms4edition/PriorityQueue.java
+++ src/algorithms4edition/PriorityQueue.java
@@ -58,8 +58,25 @@ public class PriorityQueue {
     }
   }
 
+  public static void sort(Comparable[] a){
+  	int N = a.length;
+  	for(int k = N/2; k>=1; k--)
+  		sink(a,k,N);
+  	while(N>1){
+  		exch(a, 1, N--);
+  		sink(a,1,N);
+  	}
+  }
+
+  private void sink(Comparable[] a, int k, int N){
+  	while(2*k<N){
+  		int j = 2*k;
+  		if(less)
+  	}
+  }
+
   public static void main(String[] args){
-  	
+
   }
 
 }
