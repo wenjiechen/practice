5ce0d4a2550ce15fee1735ac9bc3eb153a7bbfc5
wenjie chen <wenjie.chen@nyu.edu>
2014-01-28 20:20:48 -0500

    5.2 ;

--- src/chapter5bitmanipulation/BitQuestion.java
+++ src/chapter5bitmanipulation/BitQuestion.java
@@ -1,20 +1,53 @@
 package chapter5bitmanipulation;
 
+
 public class BitQuestion {
   /**
    * question 5.1
    */
   public static int updateBits(int n, int m, int i, int j){
-  	// create 1111000000 for j
-  	int left = ~((1<<(j+1)) -1);
-  	// create 0000000111 for i
-  	int right = ~((1<<(i))-1);
-  	//create 111100011
-  	int maskji = left | right;
-  	//clean bit i to j in n
-  	int tmp = n & maskji;
-  	//
-
+    // create 1111000000 for j
+    int left = ~((1<<(j+1)) -1);
+    // create 0000000111 for i
+    int right = ~((1<<(i))-1);
+    //create 111100011
+    int maskji = left | right;
+    //clean bit i to j in n
+    int tmp = n & maskji;
+    //shife m << i
+    m = m << i;
+    return n&m;
   }
 
+
+  //question 5.2
+  public static String printBinary(double num){
+    if(num >= 1 || num <= 0)
+      return "Error";
+
+    StringBuilder binary = new StringBuilder("0.");
+    while(num > 0){
+      if(binary.length()>100)
+        return "Error";
+      
+      num = num *2;
+      if(num >= 1){
+        binary.append(1);
+        num = num -1;
+      }
+      else{
+        binary.append(0);
+      }
+    }
+    return binary.toString();
+  }
+
+  public static void test2(){
+    double num = 0.72;
+    System.out.println(printBinary(num));
+  }
+
+  public static void main(String[] args){
+    test2();
+  }
 }
